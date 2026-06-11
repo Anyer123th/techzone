4e155bb1a474b277f39d605ae7af979941a8410b
Anyer123th <anyerperalta123457@gmail.com>
2026-06-11 12:11:28 -0400

    Delete techzone1 directory

--- techzone1/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Conexión simple (proyecto escolar)
-$conn = new mysqli("localhost", "root", "", "techzone");
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
-$conn->set_charset("utf8mb4");
-?>
--- techzone1/guardar_usuario.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Archivo de apoyo (no usado). El registro se maneja en registro.php
-header("Location: registro.php");
-exit;
-?>
